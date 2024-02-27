f1d53c7d2af048f0d2f7d84db2b970144596e473
mkswagger <mathangykrishna16@gmail.com>
2024-02-27 17:02:40 +0530

    changed main view

---
+++ Socialmedia/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Socialmedia
+//
+//  Created by mathangy on 27/02/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
